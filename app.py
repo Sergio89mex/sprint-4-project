9053a142d3122c5d2ffce704c15c3ac2c6ec01eb
Sergio89mex <sergiochavez7889@outlook.com>
2024-12-12 20:53:46 -0600

    changes to code

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import plotly.express as px
+from scipy import stats
+import matplotlib.pyplot as plt
 
 df_vehicles = pd.read_csv('vehicles_us.csv')
+
+Q1 = df_vehicles['price'].quantile(0.25)
+Q3 = df_vehicles['price'].quantile(0.75)
+IQR = Q3 - Q1
+
+upper_bound = Q3 + 1.5 * IQR
+lower_bound = Q1 - 1.5 * IQR
+
+outliers = (df_vehicles['price'] < lower_bound) | (df_vehicles['price'] > upper_bound)
+
+df_vehicles_no_outliers = df_vehicles[~outliers]
 
 st.header("My Streamlit App")
 
